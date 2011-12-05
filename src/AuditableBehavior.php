d1c855297ebacc84ece27982f47f61f1eeab6337
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-12-05 13:48:46 +0100

    fixed bad bpackage name

--- src/AuditableBehavior.php
+++ src/AuditableBehavior.php
@@ -2,8 +2,7 @@
 /**
  * This file declare the AuditableBehavior class.
  *
- * @package Loopkey
- * @subpackage lib-propel-behavior
+ * @package propel.generator.behavior.auditable
  * @author Julien Muetton <julien_muetton@carpe-hora.com>
  * @copyright (c) Carpe Hora SARL 2011
  * @since 2011-11-30
