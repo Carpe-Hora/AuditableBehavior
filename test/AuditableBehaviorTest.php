33bcb00c3e66ef43f449facc87bca16798d37c2c
Julien MUETTON <julien_muetton@carpe-hora.com>
2012-03-16 03:07:19 +0100

    extract test bootstrap

--- test/AuditableBehaviorTest.php
+++ test/AuditableBehaviorTest.php
@@ -8,17 +8,6 @@
  * @license    MIT License
  */
 
-$_SERVER['PROPEL_DIR'] = dirname(__FILE__) . '/../../../../plugins/sfPropelORMPlugin/lib/vendor/propel/';
-$propel_dir = isset($_SERVER['PROPEL_DIR']) ? $_SERVER['PROPEL_DIR'] : dirname(__FILE__) . '/../../../../../plugins/sfPropelORMPlugin/lib/vendor/propel/';
-$behavior_dir = file_exists(__DIR__ . '/../src/')
-                    ? __DIR__ . '/../src'
-                    : $propel_dir . '/generator/lib/behavior/auditable';
-
-require_once $propel_dir . '/runtime/lib/Propel.php';
-require_once $propel_dir . '/generator/lib/util/PropelQuickBuilder.php';
-require_once $propel_dir . '/generator/lib/util/PropelPHPParser.php';
-require_once $behavior_dir . '/AuditableBehavior.php';
-
 /**
  * Test for AuditableBehavior
  *
